d5c162ba947bd11156586456d2cb10ccd56c8d8b
aarsh <leyakim@mail.ru>
2022-05-13 18:24:25 +0400

    FigurePainter

---
+++ src/homework/lesson7/FigurePainter.java
@@ -0,0 +1,40 @@
+package homework.lesson7;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("m");
+            }
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("m");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" m");
+            }
+            for (int j = 4; j >= i; j--) {
+                System.out.print(" *");}
+            System.out.println();
+        }
+    }
+
+
+}
+
+
