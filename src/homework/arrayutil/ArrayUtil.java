0addb40a633e36094b6ae515091e1f15a4fc689f
aarsh <leyakim@mail.ru>
2022-05-19 08:40:44 +0400

    ArrayUtil

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -2,74 +2,76 @@ package homework.arrayutil;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-        // tpel masivi bolor elementner@
-        int[] array = {45, 461, 55, 64, 160, 108, 907, -15, 54, 501, 2};
+
+        int[] array = {45, 461, 155, -64, 160, 108, 907, -15, 54, 501, 2};
+        //Տպել մասիվի բոլոր էլեմենտները։
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();
 
-//amenamec tiv
-//       int max = array[0];
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i] > max) {
-//               max = array[i];
-//           }
-//        }
-//       System.out.println(max);
+        //  Տպել մասիվի ամենամեծ թիվը։
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("max =" + max);
 
-//amenapokr tiv
-//       int min =array[0];
-//      for (int i = 0; i < array.length; i++) {
-//           if (array[i] < min) {
-//                min = array[i];
-//           }
-//       }
-//      System.out.println(min);
+        // Տպեl մասիվի ամենափոքրը թիվը:
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("min =" + min);
+        //Տպել մասիվիբոլոր զույգ էլեմենտները ։
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+        //Տպել մասիվի բոլոր կենտ էլեմենտները։
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+        //Տպել զույգերի քանակը:
+        int countofEvens = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                countofEvens++;
+            }
+        }
+        System.out.println("զույգերի քանակը - " + countofEvens++);
 
-//amboxj tveri gumar
-//      int sum = 0;
-//       for (int i = 0; i < array.length; i++) {
-//           sum = sum + array[i];
-//        }
-//       System.out.println(sum);
-//mijin tvabanakan
-//      double sum1 = 0;
-//        for (int i = 0; i < array.length; i++) {
-//            sum1 = sum1 + array[i];
-//
-//        }
-//       System.out.println(sum1 / array.length);
+        //Տպել կենտերի քանակը։
+        int countofOdss = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                countofOdss++;
+            }
+        }
+        System.out.println("կենտերի քանակը - " + countofOdss);
 
-//kenter
-//       for (int i = 0; i < array.length; i++) {
-//
-//            if (array[i] % 2 != 0  ) {
-//               System.out.println(array[i]);
-//           }
-//       }
+        //Տպել մասիվի էլեմենտների գումարը։
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println("sum = " + sum);
+        //Տպել մասիվի բոլոր թվերի միջին թվաբանականը:
+        double avg = 0;
+        for (int i = 0; i < array.length; i++) {
+            avg += array[i];
 
-//        //zuyger
-//     for (int i = 0; i < array.length; i++) {
-//            if (array[i] % 2 == 0) {
-//                System.out.println(array[i]);
-//          }
-//       }
-        //kenteri tiv
-//      int k = 0;
-//      for (int i = 0; i < array.length; i++) {
-//            if (array[i] % 2 == 0) {
-//             k++;
-//            }
-//        }
-//       System.out.println(k);
+        }
+        System.out.println("միջին թվաբանականը = " + avg / array.length);
 
-//
-//       int m = 0;
-//       for (int i = 0; i < array.length; i++) {
-//            if (array[i] % 2 != 0) {
-//                m++;
-//            }
-//        }
-//        System.out.println(m);
     }
 }
